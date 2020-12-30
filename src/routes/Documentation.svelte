511fd021832c6658368fd04877c174083f50f6c3
Petar Gyurov <pgyurov93@gmail.com>
2020-12-30 10:14:49 +0000

    Added more to docs. Added an interactive Review demo

--- src/routes/Documentation.svelte
+++ src/routes/Documentation.svelte
@@ -1,6 +1,7 @@
 <script>
   import Page from "../components/Page.svelte";
   import { onMount } from "svelte";
+  const path = require("path");
 
   onMount(async () => {
     window.$(".ui.accordion").accordion();
@@ -108,6 +109,165 @@
         automatically move the original images based on the predictions it just
         made.
       </p>
+      <h3 class="ui header">Step 2: Detection</h3>
+      <p>
+        With the configuration all done, it's time to initiate the detection
+        process.
+      </p>
+      <h2 class="ui sub header">2.1 Begin Processing</h2>
+      <div>
+        Pressing the
+        <button class="ui positive compact button">Process</button>
+        button will begin the detection process. The application will take some
+        time to initiate the model. This can take up to a minute.
+      </div>
+      <h2 class="ui sub header">2.2 Monitoring Progress</h2>
+      <p>
+        As soon as the model has been loaded, images will start being processed.
+        You can monitor the progress in the UI.
+      </p>
+      <div>
+        <div class="ui large compact label" style="margin-right: 5px;">
+          <i class="images outline icon" />
+          <span id="pos">16/123</span>
+        </div>
+        displays the current image and the total image count.
+      </div>
+      <p />
+      <div>
+        <div class="ui large compact label" style="margin-right: 5px;">
+          <i class="clock icon" /><span id="eta">00:05:36</span>
+        </div>
+        displays the estimated remaining time (HH:MM:SS). It updates after each
+        image. May not be very accurate for a small number of images.
+      </div>
+      <p />
+      <p>
+        And of course the progress bar will show the overall progress as a
+        percentage.
+      </p>
+      <h3 class="ui header">Step 3: Review</h3>
+      <p>
+        Once the detection finishes, a pop-up window will be displayed asking
+        you to continue to the Review process. You may decline and initiate the
+        Review process another time if you wish. For more information on this,
+        see the FAQ
+        <span class="ui label">
+          <i class="question icon" />How do I resume my review?
+        </span>
+      </p>
+      <h2 class="ui sub header">3.1 Interface Overview</h2>
+      <p>
+        Hover over the various user interface elements below to get a quick
+        description of what they do
+      </p>
+      <!-- UI DEMO -->
+      <div class="ui horizontal card container" style="width: 70%;">
+        <div>
+          <div
+            class="left aligned small floating ui label"
+            data-inverted=""
+            data-tooltip="Current image filename and total image count"
+            data-position="bottom center">
+            <i class="images outline icon" />
+            demo.JPG
+            <div class="detail">16/123</div>
+          </div>
+          <div
+            id="imageContainer"
+            data-inverted=""
+            data-tooltip="Progress bar showing percentage of reviewed images"
+            data-position="bottom left">
+            <img
+              class="ui large image"
+              src={path.join(process.cwd(), 'src', 'assets', 'demo.JPG')}
+              alt="demo" />
+          </div>
+        </div>
+        <div class="content">
+          <div style="height: 87%;">
+            <div class="ui aligned two column grid">
+              <div class="row">
+                <div class="left aligned column">
+                  <h3 class="ui header">Results</h3>
+                </div>
+                <div class="right aligned column">
+                  <h2 class="ui header">
+                    <button
+                      class="ui small compact icon primary button"
+                      data-inverted=""
+                      data-tooltip="Save updated classification data and move reviewed images into folders"
+                      data-position="bottom center">
+                      <i class="save icon" />
+                      Save Progress
+                    </button>
+                  </h2>
+                </div>
+              </div>
+              <div class="row" style="padding: 0">
+                <div class="column">
+                  <h4 class="ui header">Label</h4>
+                </div>
+                <div class="right aligned column">
+                  <h4 class="ui header">Confidence</h4>
+                </div>
+              </div>
+
+              <div class="row">
+                <div class="column">
+                  <div
+                    class="ui medium green horizontal label"
+                    data-inverted=""
+                    data-tooltip="The model's predicted classification"
+                    data-position="bottom center">
+                    animal
+                  </div>
+                </div>
+                <div class="right aligned column">
+                  <div
+                    class="ui medium orange horizontal label"
+                    data-inverted=""
+                    data-tooltip="The model's confidence for this classification"
+                    data-position="bottom center">
+                    67%
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div style="margin-bottom: 1em;">
+            <button
+              class="ui left small floated compact icon button"
+              data-inverted=""
+              data-tooltip="Returns to the previous image"
+              data-position="right center">
+              <i class="arrow left icon" />
+              Prev
+            </button>
+          </div>
+          <div class="ui small fluid buttons">
+            <button
+              class="ui gray button"
+              data-inverted=""
+              data-tooltip="Changes classification and goes to next image"
+              data-position="bottom center">
+              Mark as empty
+            </button>
+            <button
+              class="ui positive button"
+              data-inverted=""
+              data-tooltip="Accepts classification and goes to next image"
+              data-position="bottom center">Correct</button>
+          </div>
+        </div>
+        <div class="ui green bottom attached progress" data-percent="10">
+          <div class="bar" />
+        </div>
+      </div>
+      <!-- UI DEMO -->
+
+      <h2 class="ui sub header">3.2 Reviewing</h2>
+      <h2 class="ui sub header">3.3 Saving Progress</h2>
     </div>
 
     <div class="ui divider" />
